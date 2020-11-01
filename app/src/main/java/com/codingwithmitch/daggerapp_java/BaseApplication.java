8ad343ac122a574311964ea21403c147df31e567
Seungwon <jshtmddnjs@naver.com>
2020-11-01 21:22:15 +0900

    DaggerApplication & DaggerAppComponent

---
+++ app/src/main/java/com/codingwithmitch/daggerapp_java/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.codingwithmitch.daggerapp_java;
+
+import com.codingwithmitch.daggerapp_java.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+
+
+}
